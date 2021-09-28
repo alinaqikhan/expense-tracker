451317926698b76fc31d859cc6948c10688bdc6a
Ali Naqi Khan <alinaqikofficial@gmail.com>
2021-09-28 19:38:15 +0500

    Created useTransactions hook for all the details
    Updated charts with transactions data, displayed total expenses

--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
 import { Doughnut } from 'react-chartjs-2';
 import useStyles from './styles';
+import { useTransactions } from '../../hooks/useTransactions';
 
 export const Details = ({ title }) => {
 
     const {income, expense} = useStyles();
+    const { total, chartData } = useTransactions(title);
 
     return (
         <Card className={title === 'Income'? income:expense}>
             <CardHeader title={title}/>
             <CardContent>
-                <Typography variant="h5">$50</Typography>
-                <Doughnut data="DATA"/>
+                <Typography variant="h5">${total}</Typography>
+                <Doughnut data={chartData}/>
             </CardContent>
         </Card>
     )
